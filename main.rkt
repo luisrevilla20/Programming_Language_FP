5891b4d14381eae5ca06c4107a7941139f3b3de0
Luis <luisrevilla20@gmail.com>
2020-12-04 19:26:24 -0600

    TMA Error

--- main.rkt
+++ main.rkt
@@ -1,8 +1,9 @@
 #lang racket
 
 (require csv-reading)
+(require plot)
 
-(define csv-path "./KO.csv")
+(define csv-path "./KO2.csv")
 
 ;Parsing function of the csv
 (define read-csv
@@ -57,16 +58,14 @@
         (/ res n)
         (loop (sub1 cont) (+ res (car col)) (cdr col)))))
 
-(define (get-SMA list days)
-  (cond
-    ((null? list)
-     '()) ;; error?
-    ;((null? (cdr list))
-     ;'())
-    ( (>= (length list) days)
-     (cons  (suma-SMA list days) (get-SMA (cdr list) days)))))
-
-;(define (get-SMA2 col days)
+(define (get-SMA days)
+  (let loop
+    ([col (get-column 5)]
+     [result empty])
+    (if (>= (length col) days)
+        (loop (cdr col) (append result (list(suma-SMA col days))))
+        result)))
+        
 
 (define (sum-EMA val1 val2 days)
   (+(* val2(/ 2 (add1 days))) (* val1 (- 1 (/ 2 (add1 days))))))
@@ -74,17 +73,63 @@
 
 (define (get-EMA col days)
   (let loop
-    ([result (append empty (list (suma-SMA col days)))]
+    ([result  (list (suma-SMA col days))]
      [cont days]
      [val1 (suma-SMA col days)]
      [val2 (list-ref col (add1 days))]
      [a (length col)])
     (if (equal? a cont)
         result
-        (loop (append result (list (sum-EMA val1 val2 days))) (add1 cont) (last_element result)(list-ref col (add1 days)) (length (cdr col))) )))
+        (loop (append result (list (sum-EMA val1 val2 days))) (add1 cont) (last_element result)(list-ref col  cont) (length (cdr col))) )))
+
 #|
-(define (repeat col n)
+(define (sumatoria col n)
   (let loop
-    (
+    ([cont n]
+     [res 0]
+     [col col])
+    (if  (zero? cont)
+         res
+        (loop (sub1 cont) (+ res (car col)) (cdr col)))))
+
+(define (get-sumatoria col days)
+  (let loop
+    ([col col]
+     [result empty])
+    (if (>= (length col) days)
+        (loop (cdr col) (append result (list(sumatoria col days))))
+        result)))
 |#
 
+(define (get-factores n)
+  (let loop
+    ([n n]
+     [result (range 1 (add1 n))])
+    (if (zero? n)
+        result
+        (loop (sub1 n) (append result (list n))))))
+
+(define (get-elements col n)
+  (let loop
+    ([col col]
+     [n n]
+     [result empty])
+    (if (zero? n)
+        result
+        (loop (cdr col) (sub1 n) (append result (list (car col)))))))
+
+(define (solve-TMA baa days)
+  (define a (foldr + 0 baa))
+ (/ a (foldr + 0 (get-factores days))))
+
+(define (sum-TMA col days)
+  (let loop
+    ([list1 (get-elements col days)]
+     [list2 (get-factores (/ days 2))]
+     [result empty])
+     (if (empty? list1)
+        ; (solve-TMA result (/ days 2))
+        result
+         (loop (cdr list1) (cdr list2) (append result (list (* (car list1) (car list2))))))))
+         
+    
